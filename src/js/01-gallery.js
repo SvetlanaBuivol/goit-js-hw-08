fb710f6ec45312c063b8524d164b911796a0f6a0
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-05-08 00:02:47 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
+import SimpleLightBox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryEl.addEventListener('click', onGalleryElClick);
+
+var SimpleLightBox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+});
+
+function createGalleryItemMarkup (galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        title="${description}"
+        />
+        </a>
+        </li> 
+        `
+    }).join('');
+};
+
+function onGalleryElClick(e) {
+    e.preventDefault();
+    
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+};
