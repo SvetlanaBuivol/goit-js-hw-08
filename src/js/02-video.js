cb600a9797fe263a6fedbf15649824ec3b8ef1f0
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-05-08 12:29:48 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+function onTimeUpdate ({seconds}) {
+    localStorage.setItem("videoplayer-current-time", seconds);
+}
+    
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
