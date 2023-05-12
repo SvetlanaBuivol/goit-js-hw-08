768662348e04495b54005baf9c329b2d87af4180
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-05-12 21:39:33 +0300

    fix-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
 import Player from '@vimeo/player';
 import throttle from "lodash.throttle";
 
-
 const iframe = document.querySelector('iframe');
+
     const player = new Player(iframe);
 
     player.on('timeupdate', throttle(onTimeUpdate, 1000));
@@ -14,5 +14,11 @@ const iframe = document.querySelector('iframe');
 function onTimeUpdate ({seconds}) {
     localStorage.setItem("videoplayer-current-time", seconds);
 }
-    
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+   
+function resumeCurrentTime() {
+    if (localStorage.getItem('videoplayer-current-time')) {
+       player.setCurrentTime(localStorage.getItem("videoplayer-current-time")); 
+    }
+}
+resumeCurrentTime();
+
